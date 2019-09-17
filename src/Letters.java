3ee39d6c496d3051f39316c81a37d82ab66ab3e6
Arrow Batson <Arrow Batson@LAPTOP-3FEJDBF1>
2019-09-16 20:28:05 -0700

    fin

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,55 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		method9(); 
+		System.out.println();
+		method2();
+		method5();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		method8();
+		method3();
+		method5();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		method7();
+		method4();
+		method5();
+	}
+	public static void method9() {
+		System.out.println("Dear Aunnty,");
+	}
+	public static void method8() {
+		System.out.println("Dear Grandma,");
+	}
+	public static void method5() {
+		System.out.println("Well I won't take anymore of you time bye.");
+		System.out.println("\nSincerly,\nArrow Batson");
+	}
+	public static void method2() {
+		System.out.print("Hello there Jen how are you doing.\nHow was the move to your new place.\nI hope you are doing well.\nI am doing great.\nSchool is good and I have tons of friends.");
+		
+	}
+	public static void method3() {
+		System.out.println("Hello Gandma how are you.");
+		System.out.println("I hope you don't encounter any bears in the near future.");
+		System.out.println("Are you and grandpa doing well.");
+		System.out.println("Has grandpa somehow gotten another injure.");
+		System.out.println("Is Cali doing well.");
+	}
+	public static void method7() {
+		System.out.println("Dear Mom,");
+	}
+	public static void method4() {
+		System.out.println("Hey mom it has been a while since we have last talked.");
+		System.out.println("How are you.");
+		System.out.println("How is dad.");
+		System.out.println("How are Conner and Kim.");
+		System.out.println("I hope that you are sill living at home and not somewhere else.");
+		System.out.println("Did the tree house get finished?");
+		
 	}
 
 }
