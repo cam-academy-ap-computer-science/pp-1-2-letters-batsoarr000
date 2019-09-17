549c0b6f29e1383340b52064bf0deec33cc114bd
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-17 07:44:55 -0700

    Done

--- src/Letters.java
+++ src/Letters.java
@@ -2,55 +2,46 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		method9(); 
-		System.out.println();
-		method2();
-		method5();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		method8();
-		method3();
-		method5();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		method7();
-		method4();
-		method5();
+		Aunt();
+		Grand();
+		Mom();
 	}
-	public static void method9() {
+	public static void DearA() {
 		System.out.println("Dear Aunnty,");
 	}
-	public static void method8() {
+	public static void DearG() {
 		System.out.println("Dear Grandma,");
 	}
-	public static void method5() {
+	public static void End() {
 		System.out.println("Well I won't take anymore of you time bye.");
 		System.out.println("\nSincerly,\nArrow Batson");
 	}
-	public static void method2() {
+	public static void Aunt() {
+		DearA(); 
 		System.out.print("Hello there Jen how are you doing.\nHow was the move to your new place.\nI hope you are doing well.\nI am doing great.\nSchool is good and I have tons of friends.");
-		
+		End();
 	}
-	public static void method3() {
+	public static void Grand() {
+		DearG();
 		System.out.println("Hello Gandma how are you.");
 		System.out.println("I hope you don't encounter any bears in the near future.");
 		System.out.println("Are you and grandpa doing well.");
 		System.out.println("Has grandpa somehow gotten another injure.");
 		System.out.println("Is Cali doing well.");
+		End();
 	}
-	public static void method7() {
+	public static void DearM() {
 		System.out.println("Dear Mom,");
 	}
-	public static void method4() {
+	public static void Mom() {
+		DearM();
 		System.out.println("Hey mom it has been a while since we have last talked.");
 		System.out.println("How are you.");
 		System.out.println("How is dad.");
 		System.out.println("How are Conner and Kim.");
 		System.out.println("I hope that you are sill living at home and not somewhere else.");
 		System.out.println("Did the tree house get finished?");
-		
+		End();
 	}
 
 }
